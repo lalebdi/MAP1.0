774d343aad98c52e7e32a20c657596205f6a6c8c
lalebdi <loai.alebdi@gmail.com>
2020-08-30 13:29:39 -0400

    defined the attributes and the entitiy

---
+++ server/src/main/java/com/leahproject/beatle/domain/ProjectTask.java
@@ -0,0 +1,54 @@
+package com.leahproject.beatle.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class ProjectTask {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String summary;
+    private String acceptanceCriteria;
+    private String Status;
+
+    public ProjectTask(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getAcceptanceCriteria() {
+        return acceptanceCriteria;
+    }
+
+    public void setAcceptanceCriteria(String acceptanceCriteria) {
+        this.acceptanceCriteria = acceptanceCriteria;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
+}
+
+//here i'm defining the attributes and this entity
