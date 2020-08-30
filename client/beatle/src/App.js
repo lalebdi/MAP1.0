d43795f8622254024ec19e177e8debf55d14f9f4
lalebdi <loai.alebdi@gmail.com>
2020-08-30 17:30:27 -0400

    added the paths and new components with their layouts

--- client/beatle/src/App.js
+++ client/beatle/src/App.js
@@ -3,15 +3,20 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import ProjectBoard from './components/ProjectBoard';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import AddProjectTask from './components/ProjectTask/AddProjectTask';
 
 
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+    <div>
       <Navbar />
-      <ProjectBoard />
+      <Route exact path="/" component={ProjectBoard} />
+      <Route exact path="/addprojecttask" component={AddProjectTask} />
     </div>
+    </Router>
   );
 }
 
