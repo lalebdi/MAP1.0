39b03d8c77163ac630c12327f902e18ae8552c9f
lalebdi <loai.alebdi@gmail.com>
2020-08-30 14:20:32 -0400

    added a save method

--- server/src/main/java/com/leahproject/beatle/service/ProjectTaskService.java
+++ server/src/main/java/com/leahproject/beatle/service/ProjectTaskService.java
@@ -1,5 +1,6 @@
 package com.leahproject.beatle.service;
 
+import com.leahproject.beatle.domain.ProjectTask;
 import com.leahproject.beatle.repository.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,15 @@ public class ProjectTaskService {
 
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask saveOrUpdateProjectTask(ProjectTask projectTask){
+
+        if(projectTask.getStatus()== null || projectTask.getStatus().equals("")){
+            projectTask.setStatus("To_DO");
+        }
+
+        return projectTaskRepository.save(projectTask);
+    }
 }
 
 //the logic here
