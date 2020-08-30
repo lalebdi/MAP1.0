05f3defde452590c771ba5ce9023afd3a7979c71
lalebdi <loai.alebdi@gmail.com>
2020-08-30 17:03:11 -0400

    created the component and its layout

---
+++ client/beatle/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
+        <div className="container">
+            <a className="navbar-brand" href="Dashboard.html">
+                Project Task Tool
+            </a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
+                <span className="navbar-toggler-icon" />
+            </button>
+        </div>
+    </nav>
+    )
+}
+
+export default Navbar
