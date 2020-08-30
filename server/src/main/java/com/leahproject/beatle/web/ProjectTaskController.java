3d96356ca9cd7247e64c27ce67839ea29ca7154b
lalebdi <loai.alebdi@gmail.com>
2020-08-30 14:45:02 -0400

    added a compressed error message

--- server/src/main/java/com/leahproject/beatle/web/ProjectTaskController.java
+++ server/src/main/java/com/leahproject/beatle/web/ProjectTaskController.java
@@ -5,7 +5,13 @@ import com.leahproject.beatle.service.ProjectTaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @CrossOrigin
@@ -16,7 +22,18 @@ public class ProjectTaskController {
     private ProjectTaskService projectTaskService;
 
     @PostMapping("")
-    public ResponseEntity<?> addPTToBoard(@RequestBody ProjectTask projectTask){
+    public ResponseEntity<?> addPTToBoard(@Valid @RequestBody ProjectTask projectTask, BindingResult result){
+//        below I am compressing the error message to have just show whats missing
+        if(result.hasErrors()){
+            Map<String, String> errorMap = new HashMap<>();
+
+            for (FieldError error: result.getFieldErrors()){
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+
+            return  new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+
         ProjectTask newPT = projectTaskService.saveOrUpdateProjectTask(projectTask);
         return  new ResponseEntity<ProjectTask>(newPT, HttpStatus.CREATED);
     }
