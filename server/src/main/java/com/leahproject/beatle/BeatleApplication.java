6128b5db2c651828d5aa5f99787c07cb5cd2d95c
lalebdi <loai.alebdi@gmail.com>
2020-08-30 13:17:50 -0400

    first commit

---
+++ server/src/main/java/com/leahproject/beatle/BeatleApplication.java
@@ -0,0 +1,13 @@
+package com.leahproject.beatle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeatleApplication.class, args);
+	}
+
+}
