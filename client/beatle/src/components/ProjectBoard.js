7f1a8dc16882e8a26b2c89d0f8789f84032b54f6
lalebdi <loai.alebdi@gmail.com>
2020-09-01 09:55:28 -0400

    added a return statement to the project board

--- client/beatle/src/components/ProjectBoard.js
+++ client/beatle/src/components/ProjectBoard.js
@@ -44,10 +44,42 @@ class ProjectBoard extends Component {
                             doneItems.push(tasks[i])
                     }
                 }
+                return (
+                    <React.Fragment>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-4">
+                                    <div className="card text-center mb-2">
+                                        <div className="card-header bg-secondary text-white">
+                                            <h3>TO DO</h3>
+                                        </div>
+                                    </div>
+                                        {todoItems}
+                                </div>
+                                <div className="col-md-4">
+                                    <div className="card text-center mb-2">
+                                        <div className="card-header bg-primary text-white">
+                                            <h3>In Progress</h3>
+                                        </div>
+                                    </div>
+                                        {inProgessItems}
+                                </div>
+                                <div className="col-md-4">
+                                    <div className="card text-center mb-2">
+                                        <div className="card-header bg-success text-white">
+                                            <h3>Done</h3>
+                                        </div>
+                                    </div>
+                                        {doneItems}
+                                </div>
+                            </div>
+                        </div>
+                        </React.Fragment>
+                )
             }
         }
 
-        BoardLogic(project_tasks);
+        BoardContent = BoardLogic(project_tasks);
 
         return (
             <div className="container">
@@ -56,36 +88,7 @@ class ProjectBoard extends Component {
             </Link>
             <br />
             <hr />
-            
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-4">
-                        <div className="card text-center mb-2">
-                            <div className="card-header bg-secondary text-white">
-                                <h3>TO DO</h3>
-                            </div>
-                        </div>
-                        {todoItems}
-                    </div>
-                    <div className="col-md-4">
-                        <div className="card text-center mb-2">
-                            <div className="card-header bg-primary text-white">
-                                <h3>In Progress</h3>
-                            </div>
-                        </div>
-                            {inProgessItems}
-                    </div>
-                    <div className="col-md-4">
-                        <div className="card text-center mb-2">
-                            <div className="card-header bg-success text-white">
-                                <h3>Done</h3>
-                            </div>
-                        </div>
-                        {doneItems}
-                    </div>
-                </div>
-            </div>
-    
+            {BoardContent}
             
         </div>
         )
