0fe2baf4e895faa485c5ec581b746d517efc49ef
lalebdi <loai.alebdi@gmail.com>
2020-09-01 10:01:49 -0400

    rendered the cards content

--- client/beatle/src/components/ProjectTask/ProjectTaskItem.js
+++ client/beatle/src/components/ProjectTask/ProjectTaskItem.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react'
 
 class ProjectTaskItem extends Component {
     render() {
+        const { project_task } = this.props;
         return (
             <div className="card mb-1 bg-light">
     
                             <div className="card-header text-primary">
-                                ID: projectSequence
+                                ID: {project_task.id}
                             </div>
                             <div className="card-body bg-light">
-                                <h5 className="card-title">summary</h5>
+                                <h5 className="card-title">{project_task.summary}</h5>
                                 <p className="card-text text-truncate ">
-                                    acceptanceCriteria
+                                    {project_task.acceptanceCriteria}
                                 </p>
                                 <a className="btn btn-primary">
                                     View / Update
