9f58ce7d7075151d6ec09608ef7e9d35560d62e7
lalebdi <loai.alebdi@gmail.com>
2020-09-08 14:42:32 -0400

    connected to the cloud server

--- client/beatle/src/actions/projectTaskActions.js
+++ client/beatle/src/actions/projectTaskActions.js
@@ -8,7 +8,7 @@ import { GET_PROJECT_TASK } from './types'
 
 export const addProjectTask = (project_task, history) => async dispatch => {
     try{
-        await axios.post("http://localhost:8080/api/board", project_task);
+        await axios.post("https://calm-spire-91306.herokuapp.com/api/board", project_task);
     history.push("/")
     dispatch({ //this is to clear out the error object when everything goes well
         type: GET_ERRORS,
@@ -30,7 +30,7 @@ export const addProjectTask = (project_task, history) => async dispatch => {
 // If theres an error it goes from here to the reducer and the reducer will return the payload which will go to the root reducer and return to update the store
 
 export const getBacklog = () => async dispatch => {
-    const res = await axios.get('http://localhost:8080/api/board/all')
+    const res = await axios.get('https://calm-spire-91306.herokuapp.com/api/board/all')
     dispatch({
         type: GET_PROJECT_TASKS,
         payload: res.data
@@ -40,7 +40,7 @@ export const getBacklog = () => async dispatch => {
 export const deleteProjectTask = pt_id => async dispatch => {
     if(window.confirm(`You are deleting project task ${pt_id}, This cannot be undone`)){
 
-        await axios.delete(`http://localhost:8080/api/board/${pt_id}`);
+        await axios.delete(`https://calm-spire-91306.herokuapp.com/api/board/${pt_id}`);
         dispatch({
             type: DELETE_PROJECT_TASK,
             payload: pt_id //
@@ -50,7 +50,7 @@ export const deleteProjectTask = pt_id => async dispatch => {
 
 export const getProjectTask = (pt_id, history) => async dispatch => {
     try{
-        const res = await axios.get(`http://localhost:8080/api/board/${pt_id}`);
+        const res = await axios.get(`https://calm-spire-91306.herokuapp.com/api/board/${pt_id}`);
     dispatch({
         type: GET_PROJECT_TASK,
         payload: res.data
